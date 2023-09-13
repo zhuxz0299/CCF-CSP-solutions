4dd704ff1eaf9cd0f01d4a552942a9dc90e42238
Xiaozhi Zhu <zhuxiaozhi@sjtu.edu.cn>
2023-09-13 09:02:46 +0800

    202203-3 begin

---
+++ 202203-3/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int l[1000]; // 计算节点到可用区的对应
+
+int main()
+{
+    int n, m; // 计算节点数目、可用区的数目
+    int g;//计算任务的组数
+
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &(l[i]));
+}
